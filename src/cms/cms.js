123fb0397d232d2d0eb3a8a3bd15889803c90fc1
rhartman99 <rhartman1239@gmail.com>
2021-10-07 14:55:54 -0400

    Test

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -1,6 +1,6 @@
 import CMS from "netlify-cms-app";
-import IndexPreview from "./previews/IndexPreview";
-import FooterPreview from "./previews/FooterPreview";
+// import IndexPreview from "./previews/IndexPreview";
+// import FooterPreview from "./previews/FooterPreview";
 
-CMS.registerPreviewTemplate("home", IndexPreview);
-CMS.registerPreviewTemplate("footer", FooterPreview);
+// CMS.registerPreviewTemplate("home", IndexPreview);
+// CMS.registerPreviewTemplate("footer", FooterPreview);
